b30e48f977d9ec1037b67449afd342fdc36d4a16
danyaffe <dan.yaffe@mail.huji.ac.il>
2021-12-26 22:29:37 +0200

    Improvement of Tree.java Added tree creation to game manager. Added Layer constants.

--- src/Pepse/PepseGameManager.java
+++ src/Pepse/PepseGameManager.java
@@ -1,10 +1,12 @@
 package Pepse;
 
+import Pepse.world.Block;
 import Pepse.world.Sky;
 import Pepse.world.Terrain;
 import Pepse.world.daynight.Night;
 import Pepse.world.daynight.Sun;
 import Pepse.world.daynight.SunHalo;
+import Pepse.world.trees.Tree;
 import danogl.GameManager;
 import danogl.GameObject;
 import danogl.collisions.Layer;
@@ -19,21 +21,32 @@ import java.awt.*;
 
 public class PepseGameManager extends GameManager {
 
+    final int SKY_LAYER = Layer.BACKGROUND;
+    final int NIGHT_LAYER = Layer.FOREGROUND;
+    final int SUN_LAYER = Layer.BACKGROUND + 1;
+    final int HALO_LAYER = Layer.BACKGROUND + 2;
+    final int TERRAIN_LAYER = Layer.STATIC_OBJECTS;
+    final int TRUNK_LAYER = Layer.BACKGROUND + 10;
+    final int LEAVES_LAYER = Layer.BACKGROUND + 11;
 
     public static void main(String[] args) {
         new PepseGameManager().run();
     }
 
     @Override
-    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
+                               UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
-        Sky.create(this.gameObjects(), windowController.getWindowDimensions(), Layer.BACKGROUND); //TODO set sky layer
-        Night.create(gameObjects(), windowController.getWindowDimensions(), 10, Layer.FOREGROUND);
-        GameObject sun = Sun.create(windowController.getWindowDimensions(), 10, gameObjects(), Layer.BACKGROUND+1);
-        SunHalo.create(gameObjects(), sun, new Color(255, 255, 0, 20), Layer.BACKGROUND+2);
-        Terrain terrain = new Terrain(gameObjects(), Layer.STATIC_OBJECTS, windowController.getWindowDimensions(), 1);
-        terrain.createInRange(0,(int) windowController.getWindowDimensions().x());
-
+        Sky.create(this.gameObjects(), windowController.getWindowDimensions(), SKY_LAYER);
+        Night.create(gameObjects(), windowController.getWindowDimensions(), 10, NIGHT_LAYER);
+        GameObject sun = Sun.create(windowController.getWindowDimensions(), 10, gameObjects(),
+                SUN_LAYER);
+        SunHalo.create(gameObjects(), sun, new Color(255, 255, 0, 20), HALO_LAYER);
+        Terrain terrain = new Terrain(gameObjects(), TERRAIN_LAYER,
+                windowController.getWindowDimensions(), 1);
+        terrain.createInRange(0, (int) windowController.getWindowDimensions().x());
+        new Tree(gameObjects(),TRUNK_LAYER,LEAVES_LAYER, 1, Block.SIZE, terrain::groundHeightAt).createInRange(0,
+                (int) windowController.getWindowDimensions().x()); // Dan debugs tree creation
     }
 
     @FunctionalInterface
--- src/Pepse/world/trees/Tree.java
+++ src/Pepse/world/trees/Tree.java
@@ -1,58 +1,86 @@
 package Pepse.world.trees;
 
+import Pepse.util.ColorSupplier;
+import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
 
 import java.awt.*;
 import java.util.Random;
-import java.util.function.IntUnaryOperator;
+import java.util.function.Function;
 
 public class Tree {
     private static final Color TRUNK_COLOR = new Color(100, 50, 20);
     private static final Color LEAF_COLOR = new Color(50, 200, 30);
-    private static final int LEAVES_SIZE = 10;
+    private static final int CANOPY_SIZE = 15;
     private static final int TREE_MAX_HEIGHT = 20;
     private static final int TREE_MIN_HEIGHT = 15;
     private static final float TREE_PROB = 0.1f;
 
 
     private final GameObjectCollection gameObjects;
+    private int trunkLayer;
+    private int leavesLayer;
     private final Random rnd;
-    private final int blockSize;
-    private final IntUnaryOperator getTerrainHeight;
+    private final float blockSize;
+    private final Function<Float, Float> getTerrainHeight;
 
-    public Tree(GameObjectCollection gameObjects, long seed, int blockSize,
-                IntUnaryOperator getTerrainHeight) {
+    public Tree(GameObjectCollection gameObjects,int trunkLayer, int leavesLayer, long seed, float blockSize,
+                Function<Float, Float> getTerrainHeight) {
         this.gameObjects = gameObjects;
+        this.trunkLayer = trunkLayer;
+        this.leavesLayer = leavesLayer;
         this.rnd = new Random(seed);
         this.blockSize = blockSize;
         this.getTerrainHeight = getTerrainHeight;
     }
 
     public void createInRange(int minX, int maxX) {
+        int min = minX - minX % (int) this.blockSize;
+        int max = maxX + ((int) this.blockSize - maxX% (int) this.blockSize);
         boolean[] place_trees = Pepse.util.RandomHelper.weightedCoin(TREE_PROB,
-                (maxX - minX) / this.blockSize, this.rnd);
-        for (int i = minX; i < maxX; i++) {
-            if (place_trees[i]) {
-                int x = i * this.blockSize;
-                int root_height = this.getTerrainHeight.applyAsInt(x);
+                (int) ((max - min) / this.blockSize), this.rnd);
+        int index = 0;
+        for (int x = min; x < max; x += (int) this.blockSize){
+            if (place_trees[index]) {
+                float root_height = this.getTerrainHeight.apply((float)x);
 
                 // TODO Dan - Check this function call below - i think you need to change it to this:
                 // TREE_MIN_HEIGHT + this.rnd.nextInt(TREE_MAX_HEIGHT);
 
-                // int trunk_height = this.rnd.nextInt(TREE_MIN_HEIGHT, TREE_MAX_HEIGHT);
-                // make_trunk(x, root_height, trunk_height);
-                // make_leaves(x, root_height + trunk_height);
+                int trunk_height = this.rnd.nextInt(TREE_MIN_HEIGHT, TREE_MAX_HEIGHT);
+                make_trunk(x, root_height, trunk_height);
+                make_leaves(x, root_height - trunk_height); // it's a minus because y coord is upside-down
             }
+            index+=1;
         }
     }
 
     //TODO: implement make_leaves. probably without the use of Block
-    private void make_leaves(int x, int i) {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    private void make_leaves(float x, float canopyCenter) {
+        x = x/this.blockSize;
+        int canopyOffset = CANOPY_SIZE / 2;
+        for (int i = 0; i < CANOPY_SIZE; i++) {
+            for (int j = 0; j < CANOPY_SIZE; j++) {
+                Vector2 pos =
+                        new Vector2(x + i - canopyOffset, canopyCenter + j - canopyOffset).mult(this.blockSize);
+                Renderable rect = new RectangleRenderable(ColorSupplier.approximateColor(LEAF_COLOR));
+                gameObjects.addGameObject(new GameObject(pos, Vector2.ONES.mult(this.blockSize),
+                        rect),leavesLayer);
+            }
+        }
     }
 
     //TODO: implement make_trunk. probably without the use of Block
-    private void make_trunk(int root_X, int root_y, int trunk_height) {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    private void make_trunk(float root_X, float root_y, int trunkHeight) {
+        for (int i = 0; i < trunkHeight; i++) {
+            Vector2 pos = new Vector2(root_X,root_y - i*this.blockSize);
+            Renderable rect = new RectangleRenderable(ColorSupplier.approximateColor(TRUNK_COLOR));
+            GameObject trunk_obj = new GameObject(pos, Vector2.ONES.mult(this.blockSize), rect);
+            gameObjects.addGameObject(trunk_obj,trunkLayer);
+            trunk_obj.setTag("block");
+        }
     }
 }
