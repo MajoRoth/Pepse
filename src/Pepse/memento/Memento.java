1de1df0f568308870f884597a1fb4d7671c40251
danyaffe <dan.yaffe@mail.huji.ac.il>
2021-12-27 22:09:36 +0200

    implemented Memento.java

--- src/Pepse/memento/Memento.java
+++ src/Pepse/memento/Memento.java
@@ -1,4 +1,14 @@
 package Pepse.memento;
 
+import danogl.collisions.GameObjectCollection;
+
 public class Memento {
+    private final GameObjectCollection state;
+    public Memento(GameObjectCollection gameObjects){
+        this.state = gameObjects;
+    }
+
+    public GameObjectCollection getState(){
+        return this.state;
+    }
 }
