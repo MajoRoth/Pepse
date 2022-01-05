ed7c45cae0151759866c7a47112f8570927bf191
Amit Roth <amitroth@gmail.com>
2022-01-05 22:29:24 +0200

    display documentation

--- src/Pepse/display/EnergyDisplayer.java
+++ src/Pepse/display/EnergyDisplayer.java
@@ -7,16 +7,29 @@ import danogl.gui.rendering.Renderable;
 import danogl.gui.rendering.TextRenderable;
 import danogl.util.Vector2;
 
+/**
+ * handles the energy displayer at the top left of the screen
+ */
 public class EnergyDisplayer extends GameObject {
     private FloatCounter counter;
     private float currentCounter;
 
+    /**
+     * constructor
+     * @param topLeftCorner
+     * @param dimensions
+     * @param counter
+     */
     public EnergyDisplayer(Vector2 topLeftCorner, Vector2 dimensions, FloatCounter counter) {
         super(topLeftCorner, dimensions, new TextRenderable("Energy: " + Float.toString(counter.getValue())));
         this.counter = counter;
         this.currentCounter = counter.getValue();
     }
 
+    /**
+     * update the energy bar
+     * @param deltaTime
+     */
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
